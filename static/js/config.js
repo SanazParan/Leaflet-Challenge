1613afd27e43f5c3280d01075ef77014edd3eeec
SanazParan <49103152+SanazParan@users.noreply.github.com>
2019-11-03 23:24:49 -0500

    Leaflet-Challenge added

---
+++ static/js/config.js
@@ -0,0 +1,3 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoic2FuYXpwYXJhbiIsImEiOiJjazFpZ3JrMnQwMzhpM25wYXFub2l0dGIwIn0.LQQzjfgLJ8MDtklcv4ik1g";
+    //"pk.eyJ1Ijoic2FuYXpwYXJhbiIsImEiOiJjazFpZ3JrMnQwMzhpM25wYXFub2l0dGIwIn0.LQQzjfgLJ8MDtklcv4ik1g"
